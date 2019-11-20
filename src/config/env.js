99e2cbcd11849fd0cc69a672822fc5315ec517ab
hovhanneshovsepyan300 <hovhanneshovsepyan300@gmail.com>
2019-11-20 19:09:12 +0400

    api root change

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
-export const API_ROOT = 'http://tukion.herokuapp.com/api/';
+export const API_ROOT = 'https://tukion.herokuapp.com/api/';
 export const YOUTUBE_API_KEY = 'AIzaSyBJyACXLV7eMl-GwHodPeP1Ew92_7QLbtE';
 export const YOUTUBE_API_BASE_URL = 'https://www.googleapis.com/youtube/v3/';
 
